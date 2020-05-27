8e19d78d03d2ba7baaaa5d005cdf759790298716
Ashraf Ali <ashrafali0148@gmail.com>
2020-05-27 19:00:56 +0100

    MendozaCore

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Mendoza",
+    products: [
+        .executable(name: "Mendoza", targets: ["Mendoza"]),
+        .library(name: "MendozaCore", targets: ["MendozaCore"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/tcamin/KeychainAccess.git", .branch("master")),
         .package(url: "https://github.com/Subito-it/Bariloche", .branch("master")),
@@ -13,11 +17,16 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
         .package(url: "https://github.com/Ashraf-Ali-aa/Slang.git", .branch("slang-swift-package")),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.1.3"),
     ],
     targets: [
         .target(
             name: "Mendoza",
-            dependencies: ["Bariloche", "Shout", "XcodeProj", "KeychainAccess", "SourceKittenFramework", "ArgumentParser", "Slang"]
+            dependencies: ["MendozaCore"]
+        ),
+        .target(
+            name: "MendozaCore",
+            dependencies: ["Bariloche", "Shout", "XcodeProj", "KeychainAccess", "SourceKittenFramework", "ArgumentParser", "Slang", "SwiftToolsSupport"]
         ),
         .testTarget(
             name: "MendozaTests",
